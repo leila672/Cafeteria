f2e39511c0e545b67f774b533e75d38ecc39c975
laila672 <lailatariq672@gmail.com>
2022-03-02 01:26:01 +0200

    edits

--- Admin/manual_orders.php
+++ Admin/manual_orders.php
@@ -1,12 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cafeteria</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/icomoon.css"> 
+    <link rel="stylesheet" href="css/style.css"> 
+
 </head>
+
 <body>
-    <h1></h1>
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">Cafeteria<small></small></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
+                aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="oi oi-menu"></span> Menu
+            </button>
+            <div class="collapse navbar-collapse" id="ftco-nav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active"><a href="home.html" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Products</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Users</a></li>
+                    <li class="nav-item"><a href="manual_orders.php" class="nav-link">Manual order</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">checks</a></li>
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    
+    <!-- END nav -->
+
+    
+
+
 </body>
+
 </html>
