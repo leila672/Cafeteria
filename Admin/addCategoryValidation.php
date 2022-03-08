8c76f7a811069dc2a9f5c5aa6443f719f76b8ebf
mohy-dev <mohy.dev@gmail.com>
2022-03-08 03:17:46 +0200

    adding category add validation

---
+++ Admin/addCategoryValidation.php
@@ -0,0 +1,26 @@
+<?php
+
+include("errorPHPChecker.php");
+
+$newCategory = $_POST['category'];
+
+include_once("../DataBase.php");
+$tableNameProducts = "category";
+$dp = new DataBase();
+$dp->connect();
+$categoryRows = $dp->select_All($tableNameProducts);
+
+$flagExist = 0;
+foreach ($categoryRows as $cat) {
+    if ($newCategory == $cat[0]) {
+        $flagExist = 1;
+    }
+}
+
+if ($flagExist) {
+    $duplicateCategory = "This Category already exists";
+    header("Location:addCategory.php?errorCategory=$duplicateCategory");
+} else {
+    $dp->insert_Category($newCategory);
+    header("Location:addProduct.php");
+}
