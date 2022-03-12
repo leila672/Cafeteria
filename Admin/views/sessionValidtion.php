132a8437a1d36eb554e6d390b15decd47fdc288a
alaafat7albab <alaa.fathalbab@gmail.com>
2022-03-12 20:39:03 +0200

    myorders table

--- Admin/views/sessionValidtion.php
+++ Admin/views/sessionValidtion.php
@@ -2,8 +2,10 @@
 session_start();
 $userName = $_SESSION['name'];
 $userImage= $_SESSION['profile_Picture'];
+$userID= $_SESSION['id'];
 if (! isset($_SESSION['name'])) {
-    header("Location: login.php");
+   //header("Location: ../../Login/index.html");
+    include ("../../Login/index.html");
 }
 
 ?>
